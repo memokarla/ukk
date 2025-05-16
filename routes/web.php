b85676ef1167c244068f99df7649b7ac34ca8181
memokarla <arrowofdarkness2@gmail.com>
2025-05-16 14:03:12 +0700

    ya

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Group rute yang memerlukan autentikasi dan role 'siswa'
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
+    'role:siswa', // Ini akan mengirimkan 'siswa' sebagai parameter ke middleware
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 
-    Route::get('/dataPkl',App\Livewire\Pkl\Index::class)->name('pkl');
-    Route::get('/tambahDataPkl',App\Livewire\Pkl\Create::class)->name('pklCreate');
+    Route::get('/dataPkl', App\Livewire\Pkl\Index::class)->name('pkl');
+    Route::get('/tambahDataPkl', App\Livewire\Pkl\Create::class)->name('pklCreate');
 });
