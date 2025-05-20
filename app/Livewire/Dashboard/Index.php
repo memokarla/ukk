3074becebd316e28ed9aae0adf08de40d65ffb5d
memokarla <arrowofdarkness2@gmail.com>
2025-05-20 19:55:13 +0700

    pie chart done

--- app/Livewire/Dashboard/Index.php
+++ app/Livewire/Dashboard/Index.php
@@ -8,6 +8,67 @@ use Illuminate\Support\Facades\Auth;
 
 class Index extends Component
 {
+    public $chartData;
+    public $chartDataRombelA;
+    public $chartDataRombelB;
+
+    // as we know ya, saat komponen livewire dipanggil, ini akan dipanggil otomatis
+    public function mount()
+    {
+        $this->loadChartData(); // ini merupakan function yang kita panggil, setelah kita membuatnya di bawah ini
+        $this->loadChartDataRombelA(); 
+        $this->loadChartDataRombelB(); 
+    }
+
+    // ini fungsi yang kita buat, jadi namanya custom, bisa saja ambilData
+    public function loadChartData()
+    {
+        // pada model Siswa yang membaca database siswas, ambil rombel dengan data SIJA A, dan yang memiliki status_lapor_pkl true, simpah ke $sijaA_sudah
+        $sijaA_sudah = Siswa::where('rombel', 'SIJA A')->where('status_lapor_pkl', true)->count();
+
+        // pada model Siswa yang membaca database siswas, ambil rombel dengan data SIJA B, dan yang memiliki status_lapor_pkl true, simpah ke $sijaB_sudah
+        $sijaB_sudah = Siswa::where('rombel', 'SIJA B')->where('status_lapor_pkl', true)->count();
+
+        // pada model Siswa yang membaca database siswas, ambil yang memiliki status_lapor_pkl false, simpah ke $belum
+        $belum = Siswa::where('status_lapor_pkl', false)->count();
+
+        // lalu, ketiga data ini disimpan ke properti Livewire 'chartData'
+        $this->chartData = [
+            ['Kategori', 'Jumlah'],
+            ['SIJA A - Sudah', $sijaA_sudah],
+            ['SIJA B - Sudah', $sijaB_sudah],
+            ['Belum Isi', $belum],
+        ];
+    }
+
+    public function loadChartDataRombelA()
+    {
+        // pada model Siswa yang membaca database siswas, ambil rombel dengan data SIJA A, dan yang memiliki status_lapor_pkl true, simpah ke $sijaA_sudah
+        $sijaA_sudah = Siswa::where('rombel', 'SIJA A')->where('status_lapor_pkl', true)->count();
+        $sijaA_belum = Siswa::where('rombel', 'SIJA A')->where('status_lapor_pkl', false)->count();
+
+
+        $this->chartDataRombelA = [
+            ['Kategori', 'Jumlah'],
+            ['SIJA A - Sudah', $sijaA_sudah],
+            ['SIJA A - Belum', $sijaA_belum],
+        ];
+    }
+
+    public function loadChartDataRombelB()
+    {
+        // pada model Siswa yang membaca database siswas, ambil rombel dengan data SIJA A, dan yang memiliki status_lapor_pkl true, simpah ke $sijaA_sudah
+        $sijaB_sudah = Siswa::where('rombel', 'SIJA B')->where('status_lapor_pkl', true)->count();
+        $sijaB_belum = Siswa::where('rombel', 'SIJA B')->where('status_lapor_pkl', false)->count();
+
+
+        $this->chartDataRombelB = [
+            ['Kategori', 'Jumlah'],
+            ['SIJA B - Sudah', $sijaB_sudah],
+            ['SIJA B - Belum', $sijaB_belum],
+        ];
+    }
+
     public function render()
     {
         // ambil email user yang sedang login
@@ -22,6 +83,9 @@ class Index extends Component
 
         return view('livewire.dashboard.index', [
             'siswa' => $siswa,
+            'chartData' => $this->chartData,
+            'chartDataRombelA' => $this->chartDataRombelA,
+            'chartDataRombelB' => $this->chartDataRombelB,
         ]);
     }
 }
--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -3,6 +3,7 @@
 
     <div class="m-4 flex flex-col gap-y-4">
 
+        <!-- CTA -->
         <div class="rounded-lg bg-white flex justify-between shadow-sm">
             <div class="mx-6 my-4 flex justify-center items-center">
                 @if ($siswa)
@@ -40,12 +41,106 @@
 
             <div class="mx-6 my-4 flex justify-center items-center shadow-md">
                 <!-- [kondisi] ? [benar] : [salah] -->
-                <img class="rounded-xl w-full h-full object-contain" 
+                <img class="rounded-xl w-56 h-56 object-contain" 
                 src="{{ $siswa->foto && file_exists(storage_path('app/public/' . $siswa->foto)) 
                 ? asset('storage/' . $siswa->foto) 
                 : asset('storage/siswa/siswa.png') }}" 
                 alt="foto siswa" />
             </div>
+        </div>
+
+        <!-- Pie Chart -->
+        <div class="flex flex-wrap gap-4 justify-between">
+            <!-- Chart -->
+            <!-- ada id kan, nah ini untuk memberi identitas agar kelak dapat dipanggil oleh scriptnya -->
+            <div id="chart_pkl" class=""></div>
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script type="text/javascript">
+                // load library Google ChartJs
+                google.charts.load('current', { // 'current' artinya pakai versi terbaru dari library-nya
+                    packages: ['corechart'] // ini mengampil paket grafik inti seperti Pie Chart, Line Chart, dll
+                });
+
+                // lalu panggil fungsi drawChart() yang kita buat di bawah ini
+                google.charts.setOnLoadCallback(drawChart);
+
+                // fungsi utamanya adalah menggambar pie chart
+                function drawChart() {
+                    const chartData = @json($chartData); // ambil data dari logika Livewire ke JS
+
+                    const data = google.visualization.arrayToDataTable(chartData); // ubah array biasa jadi format yang dimengerti oleh Google Charts
+
+                    const options = {
+                        title: 'Status Pengisian PKL', // judul
+                        pieHole: 0.4, // ini besarnya lubang, jika tanpa maka full bunder
+                        colors: ['#4ade80', '#60a5fa', '#f87171'],  // warna, urutannya berdasarkan logika kita
+                    };
+
+                    // ini untuk buat 'kanvas' nya
+                    // jadi, seluruh kodingan di dalam script ini akan dikirim ke mana nih?
+                    // yaitu lemen HTML yang memiliki id 'chart_pkl'
+                    // nanti akan dikirim ke variable chart
+                    const chart = new google.visualization.PieChart(document.getElementById('chart_pkl'));
+
+                    // gambar chartnya sekarang
+                    // ambil data yang udah di format ('data')
+                    // ambil pengaturan (judul, lubang pie, warna) dari ('options')
+                    // lalu gambar grafiknya ke dalam elemen tadi ('chart_pkl')
+                    chart.draw(data, options);
+                }
+            </script>
+
+            <!-- Chart Rombel A -->
+            <div id="chart_pkl_rombelA" class=""></div>
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script type="text/javascript">
+                google.charts.load('current', { 
+                    packages: ['corechart'] 
+                });
+
+                google.charts.setOnLoadCallback(drawChartRombelA);
+
+                function drawChartRombelA() {
+                    const chartDataRombelA = @json($chartDataRombelA); 
+
+                    const data = google.visualization.arrayToDataTable(chartDataRombelA); 
+
+                    const options = {
+                        title: 'Status Pengisian PKL Rombel A', 
+                        pieHole: 0.4, 
+                        colors: ['#4ade80', '#60a5fa'],  
+                    };
+
+                    const chart = new google.visualization.PieChart(document.getElementById('chart_pkl_rombelA'));
+                    chart.draw(data, options);
+                }
+            </script>
+
+            <!-- Chart Rombel B -->
+            <div id="chart_pkl_rombelB" class=""></div>
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script type="text/javascript">
+                google.charts.load('current', { 
+                    packages: ['corechart'] 
+                });
+
+                google.charts.setOnLoadCallback(drawChartRombelB);
+
+                function drawChartRombelB() {
+                    const chartDataRombelB = @json($chartDataRombelB); 
+
+                    const data = google.visualization.arrayToDataTable(chartDataRombelB); 
+
+                    const options = {
+                        title: 'Status Pengisian PKL Rombel B', 
+                        pieHole: 0.4, 
+                        colors: ['#4ade80', '#60a5fa'],  
+                    };
+
+                    const chart = new google.visualization.PieChart(document.getElementById('chart_pkl_rombelB'));
+                    chart.draw(data, options);
+                }
+            </script>
         </div>
 
     </div>
