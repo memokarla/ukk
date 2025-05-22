9ff8402a5cd8b2dff082f755c5de6eba8e6a75d9
memokarla <arrowofdarkness2@gmail.com>
2025-05-22 21:41:53 +0700

    kurang fe

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-4 py-1 bg-[#FCD34D] rounded-xl my-4 mx-2 text-sm font-medium leading-5 text-white focus:outline-none transition duration-150 ease-in-out'
+            ? 'inline-flex items-center px-4 py-1 bg-[#FCD34D] rounded-xl my-4 text-sm font-medium leading-5 text-white focus:outline-none transition duration-150 ease-in-out'
             : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-yellow-300 focus:outline-none focus:text-gray-700 focus:border-yellow-300 transition duration-150 ease-in-out';
 @endphp
 
