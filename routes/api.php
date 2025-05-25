e7db3c2b25389b9f2676884861c6629f7845f361
memokarla <arrowofdarkness2@gmail.com>
2025-05-26 01:07:51 +0700

    hah

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,3 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('pkl', App\Http\Controllers\Api\PklController::class);
 Route::apiResource('siswa', App\Http\Controllers\Api\SiswaController::class);
 Route::apiResource('guru', App\Http\Controllers\Api\GuruController::class);
-Route::apiResource('industri', App\Http\Controllers\Api\IndustriController::class);
